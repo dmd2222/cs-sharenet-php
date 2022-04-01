1e558ac710cee362f311edb5b4b238ba0d42ed4e
dmd2222 <dmd20@gmx.de>
2022-04-01 20:48:36 +0200

    Update update.php

--- update.php
+++ update.php
@@ -1,13 +1,21 @@
 <?php
-//FORCE UPDTAE
-function do_update($filepath,$update_path){
 
+class updater{
+
+    static public function do_file_update($filepath,$update_path){
+
+        //check vars
+        if(is_string($filepath)<>true)die("Not a valid file path.");
+        if(is_string($update_path)<>true)die("Not a valid update path.");
        
         $response_result = file_get_contents($update_path);
+
+        //check response
+        if(is_string($response_result)<>true)die("Not a valid response result.");
         
         //precheck result
         if($response_result ==false){
-            die("Error: was not able to check update.<br>");
+            die("Error: was not able to check update.(For file: " .  $filepath . ")(Update path: " . $update_path . ")<br>");
         }
         
         $contents="";
@@ -24,8 +32,6 @@ function do_update($filepath,$update_path){
             $a = file_put_contents($filepath, $response_result);
             if($contents<>$response_result){
                 die("Error: Update needed! Please update first.");
-            }else{
-                   return true;
             }
         }else{
             return true;
@@ -33,5 +39,8 @@ function do_update($filepath,$update_path){
 
 
 }
+
+}
+
 
 ?>
